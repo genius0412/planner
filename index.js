a4b8534c4ba478e3f0b7e1ff026740ae625702a5
Genius0412 <genius0412@naver.com>
2021-01-15 14:12:28 +0900

    commit

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
-const button = document.querySelector(".info-button")
-const info = document.querySelector(".info-div")
+const newButton = document.querySelector(".info-button")
+const infoContainer = document.querySelector(".info-div")
+const loadButton = document.querySelector(".load-info")
 
 let delButton = document.querySelectorAll('.info-del')
 let weekday = document.querySelectorAll('.weekday')
 let flag = false;
+
+class PlanEvent{
+    constructor(title, weekday, start, end){
+        /* 
+        * weekday = array 
+        * if start length = 1 : time is always the same
+        * else 0~ weekday order
+        */
+
+        this.title = title
+        this.weekday = weekday
+        this.start = start
+        this.end = end;
+    }
+}
 
 const addClass = (element, className) => {
     element.className += " " + className
@@ -29,12 +45,29 @@ function deleteInfo(){
     this.parentElement.remove()
 }
 
-button.addEventListener("click", (event) => {
+const getInfo = () => {
+    const info = document.querySelectorAll(".info")
+    info.forEach(e => {
+        const title = e.children[1].children[0].children[1].value
+        let weekdayArr = new Array()
+
+        for(let i=0; i<7; i++){
+            if(checkClass(e.children[2].children[i], "button-active")) weekdayArr.push(i)
+        }
+
+        console.log(e.children[3].children[0].children[1].value)
+        console.log(e.children[3].children[0].children[3].value)
+
+        console.log(`Title: ${title}\nWeekday: ${weekdayArr}`)
+    })
+}
+
+newButton.addEventListener("click", (event) => {
     event.preventDefault()
     console.log("new info created")
-    let input = document.createElement('div')
-    input.className = "info"
-    input.innerHTML += `
+    let newInfo = document.createElement('div')
+    newInfo.className = "info"
+    newInfo.innerHTML += `
     <button class="info-del">X</button>
     <div class="info-title">
     <form onSubmit="return false;">
@@ -59,7 +92,7 @@ button.addEventListener("click", (event) => {
         <input type="time" class="time input-time end-time" name="end-time">
     </form>
 </div>`
-    info.appendChild(input)
+    infoContainer.appendChild(newInfo)
     weekday.forEach(e => {
         e.removeEventListener("click", buttonActive)
     })
@@ -75,3 +108,10 @@ button.addEventListener("click", (event) => {
         e.addEventListener("click", deleteInfo)
     })
 })
+
+loadButton.addEventListener("click", (event) => {
+    event.preventDefault()
+    getInfo()
+})
+
+
